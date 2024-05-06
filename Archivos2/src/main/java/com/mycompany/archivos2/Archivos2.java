4da31552d4ca5b4555f0cae7951b489b29fd3886
JoseU28 <jrcoronelt@ufpso.edu.co>
2024-05-06 11:53:37 -0500

    Reader y Writer

---
+++ Archivos2/src/main/java/com/mycompany/archivos2/Archivos2.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.archivos2;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Archivos2 {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("./archivo.txt");
+        file.createNewFile(); //TODO: Crear un método para crear archivos y manejar la excepcion
+        
+        FileWriter writer = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(writer);
+        
+        for(int i=0; i<10;i++){
+            pw.println("Linea "+ i);
+        }
+        
+        writer.close();
+    }
+}
